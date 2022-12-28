851a68798443a705ea600a9cd68af95f407a28fd
simple <shwncho4@gmail.com>
2022-12-29 07:20:56 +0900

    chore: test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,7 @@ cd /home/ec2-user/app
 DOCKER_APP_NAME=spring
 
 # 실행중인 blue가 있는지
-EXIST_BLUE=$(docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep running)
+EXIST_BLUE=$(docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
 
 # green이 실행중이면 blue up
 if [ -z "$EXIST_BLUE" ]; then
