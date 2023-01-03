57848dc07035adc562e19d1b477ba43f19ebe560
simple <shwncho4@gmail.com>
2023-01-03 19:19:11 +0900

    docs: User API

--- src/test/java/com/server/cogito/user/controller/UserControllerTest.java
+++ src/test/java/com/server/cogito/user/controller/UserControllerTest.java
@@ -11,15 +11,20 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.http.HttpHeaders;
 import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.ResultActions;
 
+import static com.server.cogito.support.restdocs.RestDocsConfig.field;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.nullValue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
@@ -40,8 +45,9 @@ class UserControllerTest extends RestDocsSupport {
         UserResponse response = UserResponse.from(user);
         given(userService.getMe(any())).willReturn(response);
         //when
-        ResultActions resultActions = mockMvc.perform(
-                get("/api/users/me"));
+        ResultActions resultActions = mockMvc.perform(get("/api/users/me")
+                        .header(HttpHeaders.AUTHORIZATION,"Bearer testAccessToken"));
+
 
         //then
         resultActions
@@ -51,6 +57,9 @@ class UserControllerTest extends RestDocsSupport {
                 .andExpect(jsonPath("$.score",is(1)))
                 .andExpect(jsonPath("$.introduce").value(nullValue()))
                 .andDo(restDocs.document(
+                        requestHeaders(
+                                headerWithName(HttpHeaders.AUTHORIZATION).description("JWT Access Token").attributes(field("constraints", "JWT Access Token With Bearer"))
+                        ),
                         responseFields(
                                 fieldWithPath("nickname").type(JsonFieldType.STRING).description("유저 닉네임"),
                                 fieldWithPath("profileImgUrl").type(JsonFieldType.NULL).description("유저 프로필 사진"),
