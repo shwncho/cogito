1caa66a19176379ce9404cb6e44d8355fa1815f3
simple <shwncho4@gmail.com>
2023-02-12 18:25:12 +0900

    refactor: test

--- src/main/java/com/server/cogito/notification/controller/NotificationController.java
+++ src/main/java/com/server/cogito/notification/controller/NotificationController.java
@@ -4,7 +4,6 @@ import com.server.cogito.common.security.AuthUser;
 import com.server.cogito.notification.dto.NotificationResponses;
 import com.server.cogito.notification.service.NotificationService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
@@ -18,12 +17,9 @@ public class NotificationController {
     private final NotificationService notificationService;
 
     @GetMapping(value = "/subscribe", produces = "text/event-stream")
-    public ResponseEntity<SseEmitter> subscribe(@AuthenticationPrincipal AuthUser authUser,
-                                               @RequestParam(value = "lastEventId", required = false, defaultValue = "") String lastEventId) {
-        return ResponseEntity
-                .ok()
-                .header("X-Accel-Buffering","no")
-                .body(notificationService.subscribe(authUser, lastEventId));
+    public SseEmitter subscribe(@AuthenticationPrincipal AuthUser authUser,
+                                @RequestParam(value = "lastEventId", required = false, defaultValue = "") String lastEventId) {
+        return notificationService.subscribe(authUser, lastEventId);
     }
 
 
